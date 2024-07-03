3639ecbef7a579bd82c7735692fcd3e70090f5b3
Filipin <160949589+Filipe-marcelino@users.noreply.github.com>
2024-07-03 10:17:15 -0300

    Add files via upload

---
+++ revisão_dnv/copia_lista.py
@@ -0,0 +1,9 @@
+semana = ['dom','seg','ter','qua','qui','sex','sab']
+
+fim_de_semana = semana[:]
+
+del fim_de_semana[1:5]
+
+print('FDS: ',fim_de_semana)
+
+print('semana: ',semana)
---
+++ revisão_dnv/rk.py
@@ -0,0 +1,10 @@
+semana = ['dom','seg','ter','qua','qui','sex','sab']
+
+print(semana[1:6:1])
+
+print(semana[0:3:1])
+
+print(semana[4:7:1])
+
+print(semana[::2])
+#opa, omitir uns pedaços, o python se localiza pelos pontos
