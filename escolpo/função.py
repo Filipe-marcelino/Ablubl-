b387338fd086e5a278d2de89559116b691f60bbc
Filipin <160949589+Filipe-marcelino@users.noreply.github.com>
2024-11-06 08:56:46 -0300

    Add files via upload

---
+++ escolpo/função.py
@@ -0,0 +1,32 @@
+def alterar(x):
+    x = x + 1
+    print(f'{x} (alterado na função)')
+
+
+class MinhaClasse:
+    def __init__(self, x):
+        self.x = x
+        print(f'{self.x} (criei self.x)')
+        self.x = x + 1
+        print(f'{self.x} (alterei self.x)')
+        x = self.x + 1
+        print(f'{x} (alterei x na classe)')
+        alterar(x)
+        print(f'{x} (chamei alterar(x) na classe)')
+
+
+x = 1
+print(f'{x} (no começo)')
+
+
+alterar(x)
+print(f'{x} (voltei do alterar(x))')
+
+
+meu_obj = MinhaClasse(x)
+print(f'{meu_obj.x} (exibição do meu_obj)')
+
+
+print(f'{x} (fim)')
+
+
